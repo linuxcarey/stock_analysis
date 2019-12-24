0291cee3a6f08daf755f51034db7b86d53153c46
V <C>
2019-12-24 13:12:27 +0000

    Update Dashboard Part 1

--- v1_dbrd_app.R
+++ v1_dbrd_app.R
@@ -60,6 +60,46 @@ ui <- navbarPage(
                , tableOutput("stat02rsn")
                , tags$strong("Action")
                , tableOutput("stat02actn")
+               , tags$p()
+               # 4
+               , tags$strong("4. Average Daily Volume (shares)")
+               , tags$p()
+               , tableOutput("stat03")
+               , tags$p()
+               , tags$strong("Reason")
+               , tableOutput("stat03rsn")
+               , tags$strong("Action")
+               , tableOutput("stat03actn")
+               , tags$p()
+               # 5
+               , tags$strong("5. Fundamental Health Grade")
+               , tags$p()
+               , tableOutput("stat04")
+               , tags$p()
+               , tags$strong("Reason")
+               , tableOutput("stat04rsn")
+               , tags$strong("Action")
+               , tableOutput("stat04actn")
+               , tags$p()
+               # 6
+               , tags$strong("6. Got Growth?")
+               , tags$p()
+               , tableOutput("stat05")
+               , tags$p()
+               , tags$strong("Reason")
+               , tableOutput("stat05rsn")
+               , tags$strong("Action")
+               , tableOutput("stat05actn")
+               , tags$p()
+               # 7
+               , tags$strong("7. Institutional Ownership")
+               , tags$p()
+               , tableOutput("stat06")
+               , tags$p()
+               , tags$strong("Reason")
+               , tableOutput("stat06rsn")
+               , tags$strong("Action")
+               , tableOutput("stat06actn")
              )
            )
   ),
@@ -139,16 +179,16 @@ server <- function(input, output) {
     
   )
   output$stat01rsn <- renderTable( 
-paste("Valuation check. A stock with a P/S above 10 is momentum priced.
-      Buying momentum priced stocks is only recommended in a strong market.")
-, colnames = FALSE
-)
+    paste("Valuation check. A stock with a P/S above 10 is momentum priced.
+          Buying momentum priced stocks is only recommended in a strong market.")
+    , colnames = FALSE
+    )
   
   output$stat01actn <- renderTable(
-paste('O.K. to buy if P/S is less than 10. P/S ratios between 3 and 5 are best for growth stocks.
-      Ratios below 2 reflect value priced stocks.')
-, colnames = FALSE
-)
+    paste('O.K. to buy if P/S is less than 10. P/S ratios between 3 and 5 are best for growth stocks.
+          Ratios below 2 reflect value priced stocks.')
+    , colnames = FALSE
+    )
   # Price to Cash Flow
   output$stat02 <- renderTable(
     paste0("https://www.marketwatch.com/investing/Stock/",input$symb,"/profile/") %>%
@@ -159,17 +199,108 @@ paste('O.K. to buy if P/S is less than 10. P/S ratios between 3 and 5 are best f
       html_text() %>% 
       enframe(name = NULL) 
     %>% dplyr::select(" " = value)
-    )
+  )
   output$stat02rsn <- renderTable(
-paste("Companies with positive operating cash flow 
-      are safer investments than cash burners (negative cash flow).")
-, colnames = FALSE
-)
+    paste("Companies with positive operating cash flow 
+          are safer investments than cash burners (negative cash flow).")
+    , colnames = FALSE
+    )
   
   output$stat02actn <- renderTable(
     paste('O.K. to buy if Cash Flow is a positive number.')
     , colnames = FALSE
+  )
+  # Avergae daily shares
+  output$stat03 <- renderTable(
+    paste0("https://finance.yahoo.com/quote/", input$symb, "/key-statistics?p=", input$symb) %>%
+      read_html() %>%
+      html_table() %>%
+      purrr::map_df(dplyr::bind_cols) %>%
+      # Transpose
+      t() %>%
+      as_tibble() 
+    %>% dplyr::select(" " = V17)
+    
+  )
+  output$stat03rsn <- renderTable(
+    paste("Institutional buying is an important catalyst for share price growth.
+          Institutions buy hundreds of thousands of shares and prefer stocks with
+          large daily trading volumes so they can easily move in and out of positions.")
+    , colnames = FALSE
     )
+  
+  output$stat03actn <- renderTable(
+    paste('O.K. to buy if Average Daily Volume is 150,000 shares or higher, and above one million shares is best.')
+    , colnames = FALSE
+  )
+  # Health grade
+  output$stat04 <- renderTable(
+    paste("https://investorplace.com/stock-quotes/", input$symb, "-stock-quote/") %>%
+      read_html() %>%
+      html_nodes(xpath='//*[@class="stock-aside"]') %>%
+      html_nodes(xpath='//*[@class="grade grade-color"]') %>%
+      html_nodes(xpath='//*[@class="grade-color stock-grade-rating"]') %>%
+      html_text() %>% 
+      enframe(name = NULL)
+    
+  )
+  output$stat04rsn <- renderTable(
+    paste("Invest, don’t gamble! Stick with companies with solid fundamentals.")
+    , colnames = FALSE
+  )
+  
+  output$stat04actn <- renderTable(
+    paste('O.K. to buy if Fundamental Grade = A, B or C.')
+    , colnames = FALSE
+  )
+  # Got growth?
+  output$stat05 <- renderTable(
+    paste0("https://finance.yahoo.com/quote/", input$symb, "/key-statistics?p=", input$symb) %>%
+      read_html() %>%
+      html_table() %>%
+      purrr::map_df(dplyr::bind_cols) %>%
+      # Transpose
+      t() %>%
+      as_tibble() 
+    %>% dplyr::select(" " = V46)
+    
+  )
+  output$stat05rsn <- renderTable(
+    paste("Consistent strong sales growth over extended periods translates 
+          to long term stock price appreciation.")
+    , colnames = FALSE
+    )
+  
+  output$stat05actn <- renderTable(
+    paste("O.K. to buy if recent quarterly growth numbers are 8% min. (higher is better). 
+Best case is when year-over-year (YoY) growth is accelerating.")
+    , colnames = FALSE
+  )
+  # Institutional ownership
+  output$stat06 <- renderTable(
+    paste0("https://finance.yahoo.com/quote/", input$symb, "/key-statistics?p=", input$symb) %>%
+      read_html() %>%
+      html_table() %>%
+      purrr::map_df(dplyr::bind_cols) %>%
+      # Transpose
+      t() %>%
+      as_tibble() 
+    %>% dplyr::select(" " = V22)
+    
+  )
+  output$stat06rsn <- renderTable(
+    paste("Lack of institutional ownership means mutual funds, pension plans and other institutional
+             buyers don’t think they will make money owning the stock.
+             Why would you want to own it?")
+    , colnames = FALSE
+    )
+  
+  output$stat06actn <- renderTable(
+    paste("O.K. to buy if percent held by institutions is at 
+          least 30% of shares outstanding.")
+    , colnames = FALSE
+    )
+  
 }
 
 # Run the app
